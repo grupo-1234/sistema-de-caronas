13c8831e5f9dccbffaa9b410e3d9b3b0a09d427f
Milena Guerra <milenaoguerra5@gmail.com>
2026-01-22 11:13:25 -0300

    criação viagem

--- src/viagem/viagem.module.ts
+++ src/viagem/viagem.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Viagem } from './entities/viagem.entity';
-
-
+import { ViagemController } from './controllers/viagem.controller';
+import { ViagemService } from './services/viagem.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Viagem])
-  ],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Viagem])],
+  controllers: [ViagemController],
+  providers: [ViagemService],
+  exports: [],
 })
 export class ViagemModule {}
