26af893325ad482dc6892c290375d3f31ef5c150
Larissa <larisouzadev@gmail.com>
2026-01-22 11:27:04 -0300

    finalizado motorista controller e motorista module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Viagem } from './viagem/entities/viagem.entity';
 import { ViagemModule } from './viagem/viagem.module';
+import { Motorista } from './motorista/entities/motorista.entity';
+import { MotoristaModule } from './motorista/motorista.module';
 
 @Module({
   imports: [
@@ -12,10 +14,12 @@ import { ViagemModule } from './viagem/viagem.module';
         username: 'root',
         password: 'root',
         database: 'db_caronas',
-        entities: [Viagem],
+        entities: [Viagem, Motorista],
         synchronize: true,
 }),
-ViagemModule],
+ViagemModule,
+MotoristaModule
+],
   controllers: [],
   providers: [],
 })
--- src/motorista/motorista.module.ts
+++ src/motorista/motorista.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Motorista } from "./entities/motorista.entity";
+import { MotoristaService } from "./services/motorista.service";
+import { MotoristaController } from "./controllers/motorista.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Motorista])],
-    providers: [],
-    controllers: [],
+    providers: [MotoristaService],
+    controllers: [MotoristaController],
     exports: [],
 })
 
